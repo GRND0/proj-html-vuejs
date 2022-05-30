9206f61efaa4cadb3ab756536a6a0d9430eb7797
GRND0 <zefiro.turchetta@gmail.com>
2022-05-30 15:42:42 +0200

    aggiunta ulteriori sezioni alla bozza main

--- src/components/CustomMain.vue
+++ src/components/CustomMain.vue
@@ -3,30 +3,47 @@
     <div class="container-fluid">
       <div class="wrapper-interno">
         <div class="row d-flex align-items-center" id="statistiche">
-          <div class="col h-50 d-flex justify-content-center">
-            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
-            <p>1926</p>
+          <div class="col h-50 box-statistiche text-center">
+            <p class="testo-verde">1926</p>
             <p>finished sessions</p>
-            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
           </div>
-          <div class="col h-50 d-flex">
-
-            <p>100%</p>
+          <div class="col h-50 box-statistiche text-center">
+            <p class="testo-verde">100%</p>
             <p>satisfaction rate</p>
-            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
           </div>
-          <div class="col h-50 d-flex">
-
-            <p>3092+</p>
+          <div class="col h-50 box-statistiche text-center">
+            <p class="testo-verde">3092+</p>
             <p>enrolled learners</p>
-            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
           </div>
-          <div class="col h-50 d-flex">
-
-            <p>200</p>
+          <div class="col h-50 box-statistiche-last text-center">
+            <p class="testo-verde">200</p>
             <p>online instructors</p>
-            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="wrapper-interno">
+      <div class="row">
+        <div class="col">
+          <p>ABOUT ME</p>
+          <p>how to be an alpha male: the 15 habits of true alphas</p>
+        </div>
+        <div class="col">
+          <p>Want to learn how to be an alpha male? How to be more masculine? How to be the man you've always wanted to
+            be?</p>
+          <p>Your goal is to show the consistency of business, core values, and customer care. Punctuality,
+            responsability, individuality, accuracy, enthusiasm - you have to clearly explain how these qualities are
+            applicable to your work. And the important thing is you do love what you do.</p>
+        </div>
+      </div>
+    </div>
+    <div class="wrapper-interno">
+      <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col">
+          
         </div>
       </div>
     </div>
@@ -45,5 +62,28 @@ export default {
 #statistiche {
   width: 100%;
   height: 300px;
+
+
+
+  .box-statistiche {
+    background-image: url(../assets/img/background-pattern-grid-line-06.png);
+    background-repeat: no-repeat;
+  }
+
+  .box-statistiche-last {
+    background-image: url(../assets/img/background-pattern-grid-line-06.png);
+
+  }
+
+  p {
+    text-transform: uppercase;
+  }
+
+  .testo-verde {
+    color: #20ad96;
+    font-size: 3rem;
+    font-weight: 700;
+  }
+
 }
 </style>
