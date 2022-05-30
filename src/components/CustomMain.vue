ce92d0f6caa8f7fb5d8ce9a32995bb386da0f83f
GRND0 <zefiro.turchetta@gmail.com>
2022-05-30 16:53:51 +0200

    aggiunta ulteriori sezioni alla bozza main

--- src/components/CustomMain.vue
+++ src/components/CustomMain.vue
@@ -1,52 +1,189 @@
 <template>
   <div>
-    <div class="container-fluid">
+    <section class="mb-5">
+      <div class="container-fluid">
+        <div class="wrapper-interno">
+          <div class="row d-flex align-items-center" id="statistiche">
+            <div class="col h-50 box-statistiche text-center">
+              <p class="testo-verde">1926</p>
+              <p>finished sessions</p>
+            </div>
+            <div class="col h-50 box-statistiche text-center">
+              <p class="testo-verde">100%</p>
+              <p>satisfaction rate</p>
+            </div>
+            <div class="col h-50 box-statistiche text-center">
+              <p class="testo-verde">3092+</p>
+              <p>enrolled learners</p>
+            </div>
+            <div class="col h-50 box-statistiche-last text-center">
+              <p class="testo-verde">200</p>
+              <p>online instructors</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="wrapper-interno">
-        <div class="row d-flex align-items-center" id="statistiche">
-          <div class="col h-50 box-statistiche text-center">
-            <p class="testo-verde">1926</p>
-            <p>finished sessions</p>
+        <div class="row">
+          <div class="col">
+            <p>ABOUT ME</p>
+            <p>how to be an alpha male: the 15 habits of true alphas</p>
           </div>
-          <div class="col h-50 box-statistiche text-center">
-            <p class="testo-verde">100%</p>
-            <p>satisfaction rate</p>
-          </div>
-          <div class="col h-50 box-statistiche text-center">
-            <p class="testo-verde">3092+</p>
-            <p>enrolled learners</p>
-          </div>
-          <div class="col h-50 box-statistiche-last text-center">
-            <p class="testo-verde">200</p>
-            <p>online instructors</p>
+          <div class="col">
+            <p>Want to learn how to be an alpha male? How to be more masculine? How to be the man you've always wanted
+              to
+              be?</p>
+            <p>Your goal is to show the consistency of business, core values, and customer care. Punctuality,
+              responsability, individuality, accuracy, enthusiasm - you have to clearly explain how these qualities are
+              applicable to your work. And the important thing is you do love what you do.</p>
           </div>
         </div>
       </div>
-    </div>
-    <div class="wrapper-interno">
-      <div class="row">
-        <div class="col">
-          <p>ABOUT ME</p>
-          <p>how to be an alpha male: the 15 habits of true alphas</p>
-        </div>
-        <div class="col">
-          <p>Want to learn how to be an alpha male? How to be more masculine? How to be the man you've always wanted to
-            be?</p>
-          <p>Your goal is to show the consistency of business, core values, and customer care. Punctuality,
-            responsability, individuality, accuracy, enthusiasm - you have to clearly explain how these qualities are
-            applicable to your work. And the important thing is you do love what you do.</p>
+      <div class="wrapper-interno">
+        <div class="row">
+          <div class="col position-relative">
+            <img src="../assets/img/home-movation-video-poster-670x450.jpg" alt="">
+            <img class="youtube" src="../assets/img/icon-youtube-play.png" alt="">
+          </div>
+          <div class="col d-flex flex-column justify-content-center align-items-start">
+            <p>"Every morning you have two choices: continue to sleep with your dreams, or wake up and chase them."</p>
+            <img src="../assets/img/home-movation-shape-01.png" alt="">
+            <h6>Kaixa Stark</h6>
+            <small>wingman</small>
+
+
+          </div>
         </div>
       </div>
-    </div>
-    <div class="wrapper-interno">
-      <div class="row">
-        <div class="col">
+    </section>
+    <section>
+      <div class="text-center">
+        <p>begin your journey at maxcoach</p>
+        <span>Latest Online Courses</span>
+      </div>
+      <div class="wrapper-interno">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-06-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-05-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-04-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
 
         </div>
-        <div class="col">
-          
+      </div>
+      <div class="wrapper-interno">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-03-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-02-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="../assets/img/motivation-course-01-480x298.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+                <small></small>
+                <small></small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 mx-auto">
+          <a class="btn btn-primary " href="#" role="button">get started for free</a>
         </div>
       </div>
-    </div>
+
+    </section>
+    <section>
+      <div class="wrapper-interno">
+        <div class="row">
+          <div class="col">
+            <small>book store online</small>
+            <h2>be alpha with wingman's book</h2>
+            <ul>
+              <li>
+                Help you understand yourself better
+              </li>
+              <li>
+                Revealing mature tips
+              </li>
+              <li>
+                Give the right advice
+              </li>
+              <li>
+                Fascinating examples of alpha man
+              </li>
+            </ul>
+            <button>Get Free Ebook</button>
+          </div>
+          <div class="col d-flex">
+            <div class="text-center me-2">
+              <img src="../assets/img/product-book-11-400x400.jpg" alt="">
+              <p>Alpha man by Maxcoach</p>
+              <small>$29.00</small>
+            </div>
+            <div class="text-center ms-2">
+              <img src="../assets/img/product-book-10-400x400.jpg" alt="">
+              <p> Real man 4.0 by Maxcoach</p>
+              <small>$39.00</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -84,6 +221,14 @@ export default {
     font-size: 3rem;
     font-weight: 700;
   }
+
+}
+
+.youtube {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
 }
 </style>
