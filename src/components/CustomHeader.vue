e13fc292ce8f151c97b4657abe53f11e49b7e281
GRND0 <zefiro.turchetta@gmail.com>
2022-05-30 14:51:03 +0200

    terminata bozza header, iniziata bozza main

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -1,41 +1,45 @@
 <template>
   <div id="wrapper-header" class="container-fluid">
-    <div id="barra-header" class="container-fluid">
-      <div class="row h-100 d-flex align-items-center">
-        <div id="logo" class="col h-50 d-flex justify-content-center">
-          <img class="h-100" src="../assets/img/dark-logo.png" alt="" />
-        </div>
-        <div id="nav" class="col ">
-          <ul class="d-flex justify-content-around ">
-            <li>Home</li>
-            <li>Pages</li>
-            <li>Courses</li>
-            <li>Features</li>
-            <li>Blog</li>
-            <li>Shop</li>
-          </ul>
-        </div>
-        <div id="social" class="col d-flex justify-content-center">
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-facebook-f"></i>
-          <i class="fab fa-instagram"></i>
-          <i class="fab fa-linkedin"></i>
+    <div class="wrapper-interno">
+      <div id="barra-header" class="container-fluid">
+        <div class="row h-100 d-flex align-items-center">
+          <div id="logo" class="col h-50 d-flex justify-content-start">
+            <img class="h-75" src="../assets/img/dark-logo.png" alt="" />
+          </div>
+          <div id="nav" class="col ">
+            <ul class="d-flex justify-content-around ">
+              <li>Home</li>
+              <li>Pages</li>
+              <li>Courses</li>
+              <li>Features</li>
+              <li>Blog</li>
+              <li>Shop</li>
+            </ul>
+          </div>
+          <div id="social" class="col d-flex justify-content-end">
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-linkedin"></i>
+          </div>
         </div>
       </div>
-    </div>
-    <div id="jumbotron" class="container-fluid">
-      <div class="row">
-        <div id="claim" class="col">
-          <small>i'm wingman</small>
-          <h1>peace on earth a wonderful whis but no way</h1>
-          <p>
-            Success isn't overnight. It's when everyday you get a little better
-            than the day before. It all adds up.
-          </p>
-          <button>get started for free</button>
-        </div>
-        <div id="testimonial" class="col">
-          <img src="../assets/img/home-movation-hero-image.png" alt="" />
+      <div id="jumbotron" class="container-fluid pt-3">
+        <div class="row">
+          <div id="claim" class="col d-flex flex-column justify-content-center align-items-start pe-5">
+            <small>i'm wingman</small>
+            <span>peace on earth a wonderful wish but no way</span>
+            <p>
+              Success isn't overnight. It's when everyday you get a little better
+              than the day before. It all adds up.
+            </p>
+            <a class="btn btn-primary" href="#" role="button">get started for free</a>
+
+          </div>
+          <div id="testimonial" class="col d-flex justify-content-end position-relative">
+            <img id="disegno" class="position-absolute bottom-0 end-0 h-75"  src="../assets/img/home-movation-shape-face.png" alt="">
+            <img id="uomo" src="../assets/img/home-movation-hero-image.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
@@ -51,18 +55,39 @@ export default {
 <style lang="scss">
 @import "../style/common.scss";
 
+
+
 #wrapper-header {
   height: 80vh;
-  background-image:url(../assets/img/background-pattern-wavify.png);
+  background-image: url(../assets/img/background-pattern-wavify.png);
+  background-color: #f7f7fd;
 
   #barra-header {
-    height: 50px;
+    height: 80px;
   }
 
   #jumbotron {
-    height:  calc(100% - 50px);
+    height: calc(100% - 80px);
+
+    .row {
+      height: 100%;
+    } 
+
+    small {
+      text-transform: uppercase;
+    }
+
+    span {
+      text-transform: capitalize;
+      font-size:4rem;
+    }
+
+    #disegno {
+      z-index: -1;
+    }
+
+
   }
 
 }
-
 </style>
--- src/components/CustomMain.vue
+++ src/components/CustomMain.vue
@@ -1,5 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container-fluid">
+      <div class="wrapper-interno">
+        <div class="row d-flex align-items-center" id="statistiche">
+          <div class="col h-50 d-flex justify-content-center">
+            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
+            <p>1926</p>
+            <p>finished sessions</p>
+            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
+          </div>
+          <div class="col h-50 d-flex">
+
+            <p>100%</p>
+            <p>satisfaction rate</p>
+            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
+          </div>
+          <div class="col h-50 d-flex">
+
+            <p>3092+</p>
+            <p>enrolled learners</p>
+            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
+          </div>
+          <div class="col h-50 d-flex">
+
+            <p>200</p>
+            <p>online instructors</p>
+            <img class="h-75" src="../assets/img/background-pattern-grid-line-06.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +41,9 @@ export default {
 
 <style lang="scss">
 @import "../style/common.scss";
+
+#statistiche {
+  width: 100%;
+  height: 300px;
+}
 </style>
